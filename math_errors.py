ee48594e9b8de1271fe0b1d4e827b83651b4f1ea
Dan Fornika <dfornika@gmail.com>
2019-04-25 22:08:32 -0700

    Initial commit

---
+++ math_errors.py
@@ -0,0 +1,12 @@
+#!/bin/env python
+
+import unittest
+
+class TestMathErrors(unittest.TestCase):
+    def test_00(self):
+        x = 1 + 1
+        answer = 3
+        self.assertEqual(x, answer)
+
+if __name__ == '__main__':
+    unittest.main()
