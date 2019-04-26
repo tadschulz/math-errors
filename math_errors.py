19d881bdbbab29c2fac479cf887153db3cbc4e03
Dan Fornika <dfornika@gmail.com>
2019-04-25 23:13:15 -0700

    Added more failing tests

--- math_errors.py
+++ math_errors.py
@@ -3,10 +3,47 @@
 import unittest
 
 class TestMathErrors(unittest.TestCase):
+    """
+      For each test, make an adjustment so that the value of 'x' will
+      equal the value of 'answer'
+    """
+    
     def test_00(self):
         x = 1 + 1
-        answer = 3
+        answer = 1
         self.assertEqual(x, answer)
 
+    def test_01(self):
+        x = 100 / 2
+        answer = 200
+        self.assertEqual(x, answer)
+
+    def test_02(self):
+        x = 0
+        for i in range(10):
+            x += i
+        answer = 55
+        self.assertEqual(x, answer)
+        
+    def test_03(self):
+        x = 3 + 2 + 3 / 4.
+        answer = 2
+        self.assertEqual(x, answer)
+
+    def test_04(self):
+        x = 2 ** 8
+        answer = 16
+        self.assertEqual(x, answer)
+
+    def test_05(self):
+        grocery_list = {
+            'eggs': 6.72,
+            'bread': 3.28,
+            'cheese': 8.10,
+        }
+        x = sum(grocery_list.values())
+        answer = 17.10
+        self.assertEqual(x, answer)
+            
 if __name__ == '__main__':
     unittest.main()
